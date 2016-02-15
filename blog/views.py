7824df57980a49f49f171fd4eaf0aec7eec634ff
sjacez <sjacez@gmail.com>
2016-02-15 20:05:44 +0100

    Añadimos de to

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
+
 # Create your views here.
 
 def post_list(request):
@@ -13,6 +15,7 @@ def post_detail(request,pk):
 	post= get_object_or_404(Post,pk=pk)
 	return render(request,'blog/post_detail.html',{'post':post})
 
+@login_required
 def post_new(request):
 	if request.method == "POST":
 		form=PostForm(request.POST)
@@ -25,6 +28,7 @@ def post_new(request):
 		form=PostForm()
 	return render(request,'blog/post_edit.html',{'form':form,'word':'Crear'});	
 
+@login_required
 def post_edit(request,pk):
 
 	post=get_object_or_404(Post,pk=pk)
@@ -39,6 +43,7 @@ def post_edit(request,pk):
 		form = PostForm(instance=post)
 		return render(request,'blog/post_edit.html',{'form':form,'word':'Editar'})		
 
+@login_required
 def post_delete(request,pk):
 	post=get_object_or_404(Post,pk=pk)
 	post.delete()
